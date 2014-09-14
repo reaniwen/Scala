a9e5585070789c7aef9163ab04dc21cc0024360e
rean.wen <reaniwen@gmail.com>
2014-09-13 23:14:19 -0400

    HelloWorld
    First Sync

---
+++ src/com/HelloWorld.scala
@@ -0,0 +1,10 @@
+package com
+
+/**
+ * Created by Rean on 9/13/14.
+ */
+object HelloWorld {
+  def main (args: Array[String]) :Unit =  { //: Unit is strange
+    println("hello world")
+  }
+}
---
+++ src/com/Timer.scala
@@ -0,0 +1,16 @@
+package com
+
+/**
+ * Created by Rean on 9/13/14.
+ */
+object Timer {
+  def oncePerSecond(callback: () => Unit) {
+    while (true) { callback(); Thread sleep 1000 }
+  }
+  def timeFlies() {
+    println("time flies like an arrow...")
+  }
+  def main(args: Array[String]) {
+    oncePerSecond(timeFlies)
+  }
+}
